86784bb24755de30c7173e8095e0cc278984cb83
Muhamad Luthfi Duhurudin <luthfid671@gmail.com>
2025-06-30 17:25:55 +0700

    header mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -59,54 +59,12 @@ const Header = () => {
               ? "opacity-100 translate-y-0 max-h-[600px] pointer-events-auto"
               : "opacity-0 -translate-y-2 max-h-0 pointer-events-none"
           }`}>
-            <ul className={'flex text-white flex-col space-y-4 font-semibold text-base ${showDropdown ? "show-class" : "hide-class"}'}>
+            <ul className={'flex text-white flex-col space-y-2 font-semibold text-base ${showDropdown ? "show-class" : "hide-class"}'}>
               {/* SERVICES with DROPDOWN */}
-              <li className="relative mb-0">
-              <button
-                onClick={() => setShowDropdown((prev) => !prev)}
-                className="hover:text-white flex items-center gap-1 cursor-pointer "
-              >
-                Services <i className="ri-arrow-drop-down-line text-xl"></i>
-              </button>
-              {/* DROPDOWN CONTENT */}
-                <ul className={`mt-2 bg-[#f4b033] text-white rounded-lg shadow-inner space-y-2 px-4 pb-0 py-2 transform transition-all duration-300 ease-out ${
-                  showDropdown
-                    ? "opacity-100 max-h-[500px] translate-y-0"
-                    : "opacity-0 max-h-0 -translate-y-2 pointer-events-none overflow-hidden"
-                }`}>
-                  <li className="font-semibold">Website Development</li>
-                  <li className="pl-4 text-sm">
-                    <button onClick={() => scrollToSection('frontend')} className="hover:text-orange-500">
-                      - Frontend Development
-                    </button>
-                  </li>
-                  <li className="pl-4 text-sm">
-                    <button onClick={() => scrollToSection('backend')} className="hover:text-orange-500">
-                      - Backend Development
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => scrollToSection('what_we_do')} className="hover:text-orange-500">
-                      What We Do
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => scrollToSection('archivement')} className="hover:text-orange-500">
-                      Archivement
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => scrollToSection('our_portfolio')} className="hover:text-orange-500">
-                      Portfolio
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => scrollToSection('testimoni')} className="hover:text-orange-500">
-                      Testimoni
-                    </button>
-                  </li>
-                </ul>
-              </li>
+              <li><button onClick={() => scrollToSection('what_we_do')} className="hover:text-orange-600 cursor-pointer">What We Do</button></li>
+              <li><button onClick={() => scrollToSection('archivement')} className="hover:text-orange-600 cursor-pointer">Archivement</button></li>
+              <li><button onClick={() => scrollToSection('our_portfolio')} className="hover:text-orange-600 cursor-pointer">Portfolio</button></li>
+              <li><button onClick={() => scrollToSection('testimoni')} className="hover:text-orange-600 cursor-pointer">Testimoni</button></li>
               <li><button onClick={() => scrollToSection('subscribe')} className="hover:text-orange-600 cursor-pointer">Subscribe</button></li>
               <li><button onClick={() => scrollToSection('footer')} className="hover:text-orange-600 cursor-pointer">Contact Us</button></li>
               {/* <li><button onClick={() => scrollToSection('hubungi_kami')} className="bg-[#7BD73E] text-black font-bold px-4 py-2 rounded-3xl hover:bg-green-200 hover:text-[#7BD73E] cursor-pointer">Hubungi Kami</button></li> */}
